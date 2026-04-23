50c0f840bb75ae7314f5fe0ad0651e8f8bcbbeb3
Tsunade-hime <HinNyuuHime@outlook.com>
2026-04-23 16:28:19 +0800

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1675,7 +1675,7 @@ def parse_model(d, ch, verbose=True):
     conv_modules = frozenset({SPDConv, LAE, MSBlock, FasterBlock, RFEM, FCM,PConv,
                               CAB, MSCAM, SCConv, C3GhostExp, C3XExp, C3TRExp,
                               HGBlockExp, C3SCConvExp, C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp,
-                              FuseConv, FuseConvWrapper, SW_C3k2, DEAC2f, LSC2f, DMA})
+                              FuseConv, FuseConvWrapper, SW_C3k2, DEAC2f, LSC2f, DMA, Sonic})
     # 后续所有 __init__(c1, c2, n=1, ...) 的复合模块
     conv_repeat_modules = frozenset({C3GhostExp, C3XExp, C3TRExp, HGBlockExp, C3SCConvExp,
                                      C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp, SW_C3k2, DMA,
